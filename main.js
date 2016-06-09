bb2a02e570bf8a5f2e15794095dc65b0707c83d5
Oleksandr Tkachenko <tkachenkowork@gmail.com>
2016-06-09 08:44:27 +0300

    Init project

---
+++ main.js
@@ -0,0 +1,7 @@
+// Get DOM elemnts
+var sourceElem = document.getElementById("source");
+var destinationElem = document.getElementById("destination");
+
+// Get array
+var arr = sourceElem.textContent.split(" ");
+console.log(arr);
