eaf7d8286e21defe49b896fd91934b6c969678f1
Aleksandr Tkachenko <Alexandr.Tkachenko@3shape.com>
2016-06-09 17:44:12 +0300

    use copy of json-object

--- case 2/main2.js
+++ case 2/main2.js
@@ -540,7 +540,549 @@ data = [
         "greeting": "Hello, Best Trujillo! You have 7 unread messages.",
         "favoriteFruit": "banana"
     }
-]
+];
+data1 = [
+    {
+        "_id": "57575cea14dcc0c835ba71e4",
+        "index": 0,
+        "guid": "79306dac-3fd5-4eb7-b23b-8589e95b480f",
+        "isActive": false,
+        "balance": "$1,069.27",
+        "picture": "http://placehold.it/32x32",
+        "age": 31,
+        "eyeColor": "brown",
+        "name": "Fischer Baker",
+        "gender": "male",
+        "company": "PHUEL",
+        "email": "fischerbaker@phuel.com",
+        "phone": "+1 (891) 585-2853",
+        "address": "693 Pitkin Avenue, Hemlock, Oklahoma, 4525",
+        "about": "Aliqua culpa ullamco labore proident anim aliqua officia ullamco esse in minim mollit non. Labore adipisicing nisi qui dolore exercitation fugiat minim proident excepteur sint duis minim aliqua aliqua. Eiusmod consectetur tempor exercitation exercitation cupidatat culpa mollit ipsum labore aliqua cupidatat laboris. Eu amet pariatur pariatur excepteur magna aliquip ipsum mollit nostrud veniam in do eiusmod. Amet deserunt eu esse nulla occaecat veniam laborum fugiat laboris. Excepteur excepteur sunt irure mollit esse duis.\r\n",
+        "registered": "2016-03-04T07:17:48 -02:00",
+        "latitude": 10.360109,
+        "longitude": 96.492955,
+        "tags": [
+            "aute",
+            "consequat",
+            "non",
+            "commodo",
+            "eu",
+            "magna",
+            "consequat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Cochran Berg"
+            },
+            {
+                "id": 1,
+                "name": "Cervantes Richmond"
+            },
+            {
+                "id": 2,
+                "name": "Madden Mills"
+            }
+        ],
+        "greeting": "Hello, Fischer Baker! You have 1 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "57575cead7ef623e195fc1a8",
+        "index": 1,
+        "guid": "e08f3661-20fa-472a-9993-3c6944e566e9",
+        "isActive": false,
+        "balance": "$2,076.82",
+        "picture": "http://placehold.it/32x32",
+        "age": 40,
+        "eyeColor": "brown",
+        "name": "Stacey Cannon",
+        "gender": "female",
+        "company": "ASSISTIX",
+        "email": "staceycannon@assistix.com",
+        "phone": "+1 (967) 528-3024",
+        "address": "700 Thatford Avenue, Elliott, Delaware, 9455",
+        "about": "Nostrud fugiat aliqua commodo minim aliquip officia. Elit sit quis excepteur nulla Lorem occaecat aliquip. Lorem Lorem et in aute culpa enim magna dolor ex. Est commodo laboris aliqua amet fugiat qui adipisicing Lorem minim laboris sunt adipisicing commodo et. Ad cillum ex esse ad ut. Culpa ut fugiat cillum nulla nisi.\r\n",
+        "registered": "2015-06-20T01:14:13 -03:00",
+        "latitude": 82.180647,
+        "longitude": 159.108767,
+        "tags": [
+            "consequat",
+            "culpa",
+            "fugiat",
+            "incididunt",
+            "sint",
+            "ad",
+            "do"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Alma Kirk"
+            },
+            {
+                "id": 1,
+                "name": "Beach Gray"
+            },
+            {
+                "id": 2,
+                "name": "Hood Hopper"
+            }
+        ],
+        "greeting": "Hello, Stacey Cannon! You have 7 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "57575cead90de03450145acc",
+        "index": 2,
+        "guid": "2f5c44ce-633e-4710-b3cd-0c7b58b0287d",
+        "isActive": false,
+        "balance": "$3,651.02",
+        "picture": "http://placehold.it/32x32",
+        "age": 22,
+        "eyeColor": "blue",
+        "name": "Audra Travis",
+        "gender": "female",
+        "company": "ZOMBOID",
+        "email": "audratravis@zomboid.com",
+        "phone": "+1 (951) 434-3106",
+        "address": "264 Will Place, Dragoon, Alabama, 4712",
+        "about": "Aliqua amet pariatur aute eu mollit minim. Laborum minim commodo minim sunt irure. Sint qui dolor adipisicing ut. Non dolor occaecat et esse aliquip.\r\n",
+        "registered": "2014-07-07T10:23:06 -03:00",
+        "latitude": 10.668053,
+        "longitude": -142.935776,
+        "tags": [
+            "deserunt",
+            "aliquip",
+            "occaecat",
+            "veniam",
+            "officia",
+            "consectetur",
+            "non"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Isabelle Hudson"
+            },
+            {
+                "id": 1,
+                "name": "Dawson Barton"
+            },
+            {
+                "id": 2,
+                "name": "Vicky Mason"
+            }
+        ],
+        "greeting": "Hello, Audra Travis! You have 5 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "57575ceaa5c103c58250693c",
+        "index": 3,
+        "guid": "1cf33d8f-10fc-42d3-a41b-e0c554f7e79e",
+        "isActive": true,
+        "balance": "$1,650.84",
+        "picture": "http://placehold.it/32x32",
+        "age": 40,
+        "eyeColor": "brown",
+        "name": "Marsha Strong",
+        "gender": "female",
+        "company": "GEOFORMA",
+        "email": "marshastrong@geoforma.com",
+        "phone": "+1 (864) 423-2215",
+        "address": "958 Ivan Court, Sunwest, Connecticut, 4439",
+        "about": "Culpa voluptate mollit deserunt reprehenderit adipisicing qui. Tempor officia cillum velit voluptate reprehenderit aliqua. Sunt reprehenderit consequat enim est aute ullamco quis ad qui. Do tempor do irure aute deserunt pariatur labore culpa commodo dolor aute qui in. Eiusmod in proident fugiat fugiat quis dolore eiusmod ut consequat laborum. Anim laboris do pariatur adipisicing sunt in commodo.\r\n",
+        "registered": "2015-09-15T09:41:56 -03:00",
+        "latitude": 52.574937,
+        "longitude": -8.153257,
+        "tags": [
+            "amet",
+            "cillum",
+            "deserunt",
+            "exercitation",
+            "dolore",
+            "in",
+            "sint"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Carol Hart"
+            },
+            {
+                "id": 1,
+                "name": "Burris Newman"
+            },
+            {
+                "id": 2,
+                "name": "Jeannette Head"
+            }
+        ],
+        "greeting": "Hello, Marsha Strong! You have 8 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "57575cea1b0cde91aa7e84d6",
+        "index": 4,
+        "guid": "4bea237e-d129-4e10-90df-c373d14ccfa6",
+        "isActive": true,
+        "balance": "$3,690.89",
+        "picture": "http://placehold.it/32x32",
+        "age": 31,
+        "eyeColor": "brown",
+        "name": "Dolly Barber",
+        "gender": "female",
+        "company": "FIREWAX",
+        "email": "dollybarber@firewax.com",
+        "phone": "+1 (807) 488-2345",
+        "address": "386 Gem Street, Eureka, Utah, 7660",
+        "about": "Non nisi do exercitation sit laboris incididunt aliqua aute proident incididunt non proident nisi. Qui magna incididunt laborum eiusmod aute labore tempor eu non sunt. Reprehenderit duis tempor laborum adipisicing velit eiusmod in ex quis. Veniam mollit laborum aute culpa minim duis et fugiat aliqua occaecat cupidatat nostrud incididunt.\r\n",
+        "registered": "2016-04-28T02:12:39 -03:00",
+        "latitude": 62.276701,
+        "longitude": -124.869762,
+        "tags": [
+            "ut",
+            "fugiat",
+            "consequat",
+            "aute",
+            "aliqua",
+            "in",
+            "consequat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Brown Pratt"
+            },
+            {
+                "id": 1,
+                "name": "Guadalupe Steele"
+            },
+            {
+                "id": 2,
+                "name": "Marci Mclaughlin"
+            }
+        ],
+        "greeting": "Hello, Dolly Barber! You have 6 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "57575cea5846177ce0f7987d",
+        "index": 5,
+        "guid": "ae7a0f13-cca9-4aac-89f8-0701b438a345",
+        "isActive": true,
+        "balance": "$3,453.86",
+        "picture": "http://placehold.it/32x32",
+        "age": 34,
+        "eyeColor": "brown",
+        "name": "Gutierrez Warner",
+        "gender": "male",
+        "company": "SNORUS",
+        "email": "gutierrezwarner@snorus.com",
+        "phone": "+1 (851) 463-2169",
+        "address": "679 Woodrow Court, Tonopah, Iowa, 2086",
+        "about": "Sit non occaecat deserunt cillum proident in aliqua sit officia commodo laborum cupidatat. Fugiat labore esse dolor cillum nostrud occaecat ad voluptate dolor laborum est. Dolor sint sint labore nulla ipsum quis ad sunt.\r\n",
+        "registered": "2016-01-20T11:19:06 -02:00",
+        "latitude": -64.707017,
+        "longitude": -106.34067,
+        "tags": [
+            "ipsum",
+            "commodo",
+            "aliqua",
+            "qui",
+            "id",
+            "tempor",
+            "tempor"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Irene Ellison"
+            },
+            {
+                "id": 1,
+                "name": "Concetta Roberson"
+            },
+            {
+                "id": 2,
+                "name": "Nora Zamora"
+            }
+        ],
+        "greeting": "Hello, Gutierrez Warner! You have 5 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "57575ceadb03502d643c6f0f",
+        "index": 6,
+        "guid": "6037c4bd-1b60-44c7-bea1-e08e2e76477d",
+        "isActive": true,
+        "balance": "$2,245.70",
+        "picture": "http://placehold.it/32x32",
+        "age": 28,
+        "eyeColor": "brown",
+        "name": "Dalton Tyson",
+        "gender": "male",
+        "company": "EXOSPEED",
+        "email": "daltontyson@exospeed.com",
+        "phone": "+1 (977) 597-3303",
+        "address": "663 Seaview Avenue, Swartzville, Pennsylvania, 5022",
+        "about": "Duis quis velit ex est ex est. Sint deserunt cupidatat nulla consectetur sit non non. Sint voluptate cillum id reprehenderit elit adipisicing anim laboris. Nostrud sint nulla amet eu incididunt nostrud.\r\n",
+        "registered": "2014-03-27T06:16:51 -02:00",
+        "latitude": 48.137945,
+        "longitude": -2.521632,
+        "tags": [
+            "sunt",
+            "laborum",
+            "id",
+            "laborum",
+            "nisi",
+            "aliquip",
+            "reprehenderit"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Reva Merrill"
+            },
+            {
+                "id": 1,
+                "name": "Gross Fletcher"
+            },
+            {
+                "id": 2,
+                "name": "Casandra Greene"
+            }
+        ],
+        "greeting": "Hello, Dalton Tyson! You have 1 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "57575ceaa720f6947590bde8",
+        "index": 7,
+        "guid": "2f813332-0faf-4068-9a7e-addd305d5ca2",
+        "isActive": true,
+        "balance": "$1,209.80",
+        "picture": "http://placehold.it/32x32",
+        "age": 39,
+        "eyeColor": "green",
+        "name": "Savannah Olson",
+        "gender": "female",
+        "company": "CENTICE",
+        "email": "savannaholson@centice.com",
+        "phone": "+1 (858) 506-3300",
+        "address": "624 Irving Place, Hiwasse, Virgin Islands, 6468",
+        "about": "Excepteur deserunt nulla ex deserunt et enim anim. Tempor laboris nisi est commodo pariatur excepteur voluptate. Labore proident dolor culpa dolore aliqua.\r\n",
+        "registered": "2014-07-18T12:57:34 -03:00",
+        "latitude": -14.82019,
+        "longitude": -150.509211,
+        "tags": [
+            "duis",
+            "ipsum",
+            "quis",
+            "elit",
+            "non",
+            "exercitation",
+            "proident"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Heather Schroeder"
+            },
+            {
+                "id": 1,
+                "name": "Summers Blackwell"
+            },
+            {
+                "id": 2,
+                "name": "Perry Hanson"
+            }
+        ],
+        "greeting": "Hello, Savannah Olson! You have 10 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "57575cea60cd512063b1b61d",
+        "index": 8,
+        "guid": "8e9facd1-98a5-4296-8c32-78d9e08b9b5a",
+        "isActive": false,
+        "balance": "$1,146.80",
+        "picture": "http://placehold.it/32x32",
+        "age": 26,
+        "eyeColor": "blue",
+        "name": "Erma Guerrero",
+        "gender": "female",
+        "company": "ACRUEX",
+        "email": "ermaguerrero@acruex.com",
+        "phone": "+1 (996) 442-3995",
+        "address": "327 Hall Street, Kula, American Samoa, 549",
+        "about": "Et dolore laboris qui dolore aliquip pariatur eu dolore. Magna est sit Lorem dolore consequat anim dolore Lorem eiusmod eiusmod. Elit irure labore elit eu tempor culpa. Ad do minim nostrud do minim mollit laboris mollit.\r\n",
+        "registered": "2014-02-06T05:03:40 -02:00",
+        "latitude": -38.335519,
+        "longitude": 95.643728,
+        "tags": [
+            "magna",
+            "duis",
+            "eiusmod",
+            "fugiat",
+            "ullamco",
+            "labore",
+            "proident"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Fernandez Cooke"
+            },
+            {
+                "id": 1,
+                "name": "Hilda Allison"
+            },
+            {
+                "id": 2,
+                "name": "Hays Woodard"
+            }
+        ],
+        "greeting": "Hello, Erma Guerrero! You have 9 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "57575cea24862afb127cb721",
+        "index": 9,
+        "guid": "51a14864-3600-4121-ace3-078c7b87e5da",
+        "isActive": false,
+        "balance": "$3,045.58",
+        "picture": "http://placehold.it/32x32",
+        "age": 37,
+        "eyeColor": "brown",
+        "name": "Joy Warren",
+        "gender": "female",
+        "company": "STELAECOR",
+        "email": "joywarren@stelaecor.com",
+        "phone": "+1 (884) 594-2268",
+        "address": "672 Richards Street, Teasdale, Federated States Of Micronesia, 7940",
+        "about": "Nostrud enim proident pariatur consectetur exercitation ullamco quis commodo esse. Anim voluptate ad laboris pariatur ut excepteur sit incididunt non in do. Minim mollit dolore est ad laboris consectetur excepteur tempor cupidatat. Lorem esse deserunt ex proident labore ullamco pariatur laborum pariatur est. Dolor aliquip sint sint eiusmod labore reprehenderit. Voluptate nulla cillum dolor nisi.\r\n",
+        "registered": "2015-09-20T09:10:10 -03:00",
+        "latitude": -84.023525,
+        "longitude": -135.49288,
+        "tags": [
+            "anim",
+            "sint",
+            "aliqua",
+            "laboris",
+            "aliquip",
+            "irure",
+            "amet"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Rowena Singleton"
+            },
+            {
+                "id": 1,
+                "name": "Margaret Dunlap"
+            },
+            {
+                "id": 2,
+                "name": "Maynard Knox"
+            }
+        ],
+        "greeting": "Hello, Joy Warren! You have 1 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "57575ceaf5d098d140004670",
+        "index": 10,
+        "guid": "39f34fee-602b-4f6a-ae8f-5f453bd38d1e",
+        "isActive": true,
+        "balance": "$3,611.62",
+        "picture": "http://placehold.it/32x32",
+        "age": 24,
+        "eyeColor": "brown",
+        "name": "Dean Joyce",
+        "gender": "male",
+        "company": "HOTCAKES",
+        "email": "deanjoyce@hotcakes.com",
+        "phone": "+1 (945) 424-3072",
+        "address": "288 Madison Street, Dupuyer, Alaska, 7563",
+        "about": "Esse officia qui amet aliqua enim eu commodo non pariatur occaecat veniam qui aliqua. Incididunt et mollit incididunt deserunt excepteur. Velit occaecat qui duis eiusmod voluptate minim commodo proident voluptate magna officia magna deserunt exercitation.\r\n",
+        "registered": "2016-04-23T12:12:13 -03:00",
+        "latitude": -20.548865,
+        "longitude": -44.7997,
+        "tags": [
+            "exercitation",
+            "sunt",
+            "ex",
+            "culpa",
+            "voluptate",
+            "qui",
+            "Lorem"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Powell Burke"
+            },
+            {
+                "id": 1,
+                "name": "Kirsten Keith"
+            },
+            {
+                "id": 2,
+                "name": "Carey Kent"
+            }
+        ],
+        "greeting": "Hello, Dean Joyce! You have 2 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "57575cea5bdfe8d7f0e68341",
+        "index": 11,
+        "guid": "f2b9d83b-be3e-4c02-8d35-16dda76412a5",
+        "isActive": true,
+        "balance": "$2,944.54",
+        "picture": "http://placehold.it/32x32",
+        "age": 30,
+        "eyeColor": "blue",
+        "name": "Best Trujillo",
+        "gender": "male",
+        "company": "MUSANPOLY",
+        "email": "besttrujillo@musanpoly.com",
+        "phone": "+1 (863) 474-3466",
+        "address": "613 Love Lane, Layhill, Louisiana, 8166",
+        "about": "Enim nisi in ex dolore incididunt labore occaecat sunt tempor dolor officia magna occaecat. Quis ex minim velit adipisicing officia incididunt. Elit sint labore voluptate sint excepteur in ipsum cupidatat veniam laboris voluptate. Voluptate veniam dolore voluptate in occaecat sit laborum Lorem reprehenderit. Irure nisi nisi qui proident veniam laboris. In quis labore excepteur aliqua ea do sunt.\r\n",
+        "registered": "2014-06-15T10:42:06 -03:00",
+        "latitude": 17.030462,
+        "longitude": 153.478868,
+        "tags": [
+            "consectetur",
+            "qui",
+            "reprehenderit",
+            "dolor",
+            "consectetur",
+            "cillum",
+            "ad"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Mullins Adams"
+            },
+            {
+                "id": 1,
+                "name": "Randall Pacheco"
+            },
+            {
+                "id": 2,
+                "name": "Landry Shields"
+            }
+        ],
+        "greeting": "Hello, Best Trujillo! You have 7 unread messages.",
+        "favoriteFruit": "banana"
+    }
+];
 
 // get DOM elements
 var arrayInElem = document.getElementById("arrayIn"),
@@ -557,7 +1099,7 @@ btnBubbleSort.addEventListener("click", function() {
 });
 
 btnSelectionSort.addEventListener("click", function() {
-    var arrSelectionSort = sortSelection(data);
+    var arrSelectionSort = sortSelection(data1);
     showData(arrSelectionSort, arrayOutSelectionElem);
 });
 
